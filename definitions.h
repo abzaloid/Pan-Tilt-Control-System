4052009fe8dd7213a8fe286b38a9cd996b4cc091
Abzal Serekov <abzaloid@users.noreply.github.com>
2015-01-28 11:33:58 +0600

    small update
    change Y_MEAN, Y_DEVIATION
    
    #define ORIGIN 0
    #define SIDE 1

--- definitions.h
+++ definitions.h
@@ -35,16 +35,22 @@
 #define X_DEVIATION 6000
 #define X_MEAN 0
 #define Y_DEVIATION 200
-#define Y_MEAN -1300
+#define Y_MEAN -1500
 // Time to look at and wait for the next movement
 #define MIN_FREE_TIME 2500
 #define MAX_FREE_TIME 8000
+// The state of random position
+#define ORIGIN 0
+#define SIDE 1
+
 
 /* Limitations of motor positions */
 #define ORIGIN_POS 2048
 #define MAX_POS 512
 
 #define THRESHOLD_LENGTH 50
+
+
 
 
 #endif
